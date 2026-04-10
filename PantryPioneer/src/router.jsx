b6738eba648a3eae4b09839d492cd419d9db2730
James Z <125828382+Jamziewebdev@users.noreply.github.com>
2026-04-10 15:47:03 +0200

    Refactor router to use lazy loading and Suspense for improved performance and loading experience

--- PantryPioneer/src/router.jsx
+++ PantryPioneer/src/router.jsx
@@ -1,3 +1,4 @@
+import { lazy, Suspense } from "react";
 import {
     createBrowserRouter,
     createRoutesFromElements,
@@ -5,29 +6,45 @@ import {
     Route,
 } from "react-router-dom";
 import Layout from "./components/common/Layout.jsx";
-import Home from "./pages/Home.jsx";
-import SearchPage from "./pages/SearchPage.jsx";
-import Login from "./pages/Login.jsx";
-import RecipePage from "./pages/RecipePage.jsx";
-import NotFound from "./pages/NotFound.jsx";
-import Account from "./pages/Account.jsx";
-import Pantry from "./pages/Pantry.jsx";
+import LoadingSpinner from "./components/common/LoadingSpinner.jsx";
+
+const Home = lazy(() => import("./pages/Home.jsx"));
+const SearchPage = lazy(() => import("./pages/SearchPage.jsx"));
+const Login = lazy(() => import("./pages/Login.jsx"));
+const RecipePage = lazy(() => import("./pages/RecipePage.jsx"));
+const NotFound = lazy(() => import("./pages/NotFound.jsx"));
+const Account = lazy(() => import("./pages/Account.jsx"));
+const Pantry = lazy(() => import("./pages/Pantry.jsx"));
+
+function withRouteFallback(element) {
+    return (
+        <Suspense fallback={<LoadingSpinner message="Loading page..." />}>
+            {element}
+        </Suspense>
+    );
+}
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="search" element={<SearchPage />} />
-            <Route path="recipepage" element={<RecipePage />} />
-            <Route path="recipe/:id" element={<RecipePage />} />
-            <Route path="account" element={<Account />} />
-            <Route path="pantry" element={<Pantry />} />
+            <Route index element={withRouteFallback(<Home />)} />
+            <Route path="search" element={withRouteFallback(<SearchPage />)} />
+            <Route
+                path="recipepage"
+                element={withRouteFallback(<RecipePage />)}
+            />
+            <Route
+                path="recipe/:id"
+                element={withRouteFallback(<RecipePage />)}
+            />
+            <Route path="account" element={withRouteFallback(<Account />)} />
+            <Route path="pantry" element={withRouteFallback(<Pantry />)} />
             <Route
                 path="favorites"
                 element={<Navigate to="/pantry?tab=favorites" replace />}
             />
-            <Route path="login" element={<Login />} />
-            <Route path="*" element={<NotFound />} />
+            <Route path="login" element={withRouteFallback(<Login />)} />
+            <Route path="*" element={withRouteFallback(<NotFound />)} />
         </Route>,
     ),
 );
